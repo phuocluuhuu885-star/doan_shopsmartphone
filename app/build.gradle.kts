93230de2aa86e6e599cf7b9a519977ec2f4fc9f3
NQT4192 <141619421+thangnqph44861@users.noreply.github.com>
2026-05-27 01:37:32 +0700

    gg servies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation ("io.jsonwebtoken:jjwt-jackson:0.11.2")
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
-    implementation("com.facebook.android:facebook-login:latest.release")
+
     implementation("com.google.firebase:firebase-messaging:23.4.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
